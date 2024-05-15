0db712d721819c4319f8e5b0410f681c2aa03e02
Digambar Kumbhar <digambarckumbhar299@gmail.com>
2024-05-15 12:44:55 +0530

    Initial ebook card designed

--- src/components/library/Elibrary.js
+++ src/components/library/Elibrary.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import img from 'file:///D:/Pictuers/design-book-covers-online.png';
 
 function ELibrary() {
     const [ebooks, setEbooks] = useState([]);
@@ -27,20 +28,29 @@ function ELibrary() {
     }
     return (
     <div className='libraryebook'>
-        <h1>Library Books</h1>
-        {ebooks.result && (
-            ebooks.result.map(el => (
-                <div key={el.id} className="book-container">
-                    <img className="book-image" src={el.coverImageURL} alt={el.title} />
-                    <div className="book-details">
-                        <h2 className="book-title">{el.title}</h2>
-                        <p>Author: {el.author}</p>
-                        <p className="book-description">{el.description}</p>
-                        <a href={el.fileURL}>Read Book</a>
-                    </div>
-                </div>
-            ))
-        )}
+        <div className="mid">
+            <h1>Library Books</h1>
+            <div className="ebook-grid">
+                {ebooks.result && (
+                    ebooks.result.map(el => (
+                        <div key={el.id} className="ebook-container">
+                            <div className="ghost">
+                                <img className="ebook-image" src={img} alt={el.title} />
+                                <div className="book-details">
+                                    <div>
+                                        <h2 className="book-title">{el.title}</h2>
+                                        <p className='small'>Author: {el.author}</p>
+                                        <p className="book-description">{el.description}</p>
+                                        <span className='grow'></span>
+                                        <span className='link'>Read Book</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
     </div>
     
     );
